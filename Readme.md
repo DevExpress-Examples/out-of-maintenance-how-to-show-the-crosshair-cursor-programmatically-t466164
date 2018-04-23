de60107f7d1ed08fbd98e6073faf70c624944863
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: show the Crosshair Cursor programmatically
+
+
+This example demonstrates how to show the Crosshair Cursor programmatically on a mouse click.
+
+
+<h3>Description</h3>
+
+To show the Crosshair Cursor, call the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsXYDiagram2D_ShowCrosshairtopic">XYDiagram2D.ShowCrosshair</a>&nbsp;method and specify the mouse position&nbsp;as a method parameter.
+
+<br/>
+
+
---
+++ VB/ShowCrosshairExample/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Charts
+
+Namespace ShowCrosshairExample
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        #Region "#MouseUp"
+        Private Sub OnChartControlMouseUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            Dim xyDiagram As XYDiagram2D = TryCast(chartControl.Diagram, XYDiagram2D)
+            If xyDiagram Is Nothing Then
+                Return
+            End If
+
+            xyDiagram.ShowCrosshair(e.GetPosition(chartControl))
+        End Sub
+        #End Region ' #MouseUp
+    End Class
+End Namespace
