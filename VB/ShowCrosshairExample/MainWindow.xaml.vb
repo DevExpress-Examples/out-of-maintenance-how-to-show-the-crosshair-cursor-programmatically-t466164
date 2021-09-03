b90c1b65a72331a9bbcaf457b74dbe7ce8ecb276
DevExpressExampleBot <fakeEmail>
2021-09-03 18:14:01 +0300

    Source auto update [skip ci]

--- VB/ShowCrosshairExample/MainWindow.xaml.vb
+++ VB/ShowCrosshairExample/MainWindow.xaml.vb
@@ -3,22 +3,22 @@ Imports System.Windows.Input
 Imports DevExpress.Xpf.Charts
 
 Namespace ShowCrosshairExample
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        #Region "#MouseUp"
-        Private Sub OnChartControlMouseUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-            Dim xyDiagram As XYDiagram2D = TryCast(chartControl.Diagram, XYDiagram2D)
-            If xyDiagram Is Nothing Then
-                Return
-            End If
+		#Region "#MouseUp"
+		Private Sub OnChartControlMouseUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Dim xyDiagram As XYDiagram2D = TryCast(chartControl.Diagram, XYDiagram2D)
+			If xyDiagram Is Nothing Then
+				Return
+			End If
 
-            xyDiagram.ShowCrosshair(e.GetPosition(chartControl))
-        End Sub
-        #End Region ' #MouseUp
-    End Class
+			xyDiagram.ShowCrosshair(e.GetPosition(chartControl))
+		End Sub
+		#End Region ' #MouseUp
+	End Class
 End Namespace
